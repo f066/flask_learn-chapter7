7bc1333a23d0a30068e3fceb1567282abdf18282
f066 <1920266023@qq.com>
2019-02-22 18:20:59 +0800

    8-17

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,7 +11,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(usre,form.remember_me.data)
+            login_user(user,form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
@@ -22,3 +22,15 @@ def logout():
     logout_user()
     flash('you have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('you can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
